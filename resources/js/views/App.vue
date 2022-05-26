e8e06405479cf1662ac8d0438fb5f5898b9fd682
Donatella7 <campaniellodonatella@gmail.com>
2022-05-26 15:27:24 +0200

    Set Vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container-fluid">
+        
+    </div>
+</template>
+
+<script>
+// import axios from 'axios';
+/* import PostList from '../components/PostList.vue'; */
+export default {
+    name: "App",
+    components: {
+        
+    }
+}
+</script>
+
+<style scoped>
+</style>
