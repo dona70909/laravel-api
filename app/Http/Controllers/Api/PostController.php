dc21159b2db668c8e4c7d89aee539b367d9171c0
Donatella7 <campaniellodonatella@gmail.com>
2022-05-26 15:34:24 +0200

    Index api postController index return a jsonPosts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Model\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,7 +15,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        /* //# Gett all the post */
+        $posts = Post::all();
+
+        /* //# return as json format */
+        return response()->json($posts);
     }
 
     /**
